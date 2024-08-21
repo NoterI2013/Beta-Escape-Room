a5edbb84fda265a4243bbb4bab05dbed9d1a2402
NoterI2013 <noter.i.2013@gmail.com>
2024-08-22 02:27:26 +0800

    Add: weapon seqence display

--- src/pe.js
+++ src/pe.js
@@ -1,39 +1,62 @@
-let keySequence = []; // Array to store the sequence of button presses
-const specialKey = "KEYS"; // The special key sequence you're looking for
-const maxSequenceLength = specialKey.length; // Maximum allowed length of the sequence
-
 const problemKey = "ProblemE"
 const judgeResult = document.getElementById("judge-result");
+
+let keySequence = []; // Array to store the sequence of button presses
+let weaponSequence = [];
+const maxSequenceLength = 16;
+
+const tacticTail = `<br/><br/><br/>
+                        <button class="btn btn-outline-primary btn-sm float-right" id="key-tactic-send" style="margin-top: -2em;">確認</button>
+                        <button class="btn btn-outline-primary btn-sm float-right" id="key-tactic-clear" style="margin-top: -2em;">取消</button>
+                    `;
 
 document.querySelectorAll('.weapon-btn').forEach(button => {
     button.addEventListener('click', function() {
 
         keySequence.push(button.dataset.note);
+        weaponSequence.push(button.textContent);
         if(keySequence.length > maxSequenceLength){
             keySequence.shift();
+            weaponSequence.shift();
         }
+        
+        judgeResult.innerHTML = generate('primary');
+        judgeResult.children[0].innerHTML += (weaponSequence.slice(-weaponSequence.length).join(' → ') + tacticTail);
+        document.getElementById('key-tactic-send').addEventListener('click', function() {
+            judge();
+            clearUserInput(true);
+        });
+        document.getElementById('key-tactic-clear').addEventListener('click', function() {
+            clearUserInput(true);
+        });
 
-        if(keySequence.length != maxSequenceLength){
-            judgeResult.innerHTML = "";
-            return;
-        }
+    });
+});
 
+function clearUserInput(status){
+    keySequence = [];
+    weaponSequence = [];
+    if(status === true){
+        judgeResult.innerHTML = "";
+    }
+}
+
+function judge(){
         judgeResult.innerHTML = generate("info");
         axios.post('axios/test', {
             problem: problemKey,
-            encrypt: keySequence.slice(-specialKey.length).join('')
+            encrypt: keySequence.slice(-keySequence.length).join('')
         })
         .then(function (response) {
             if(response.data.accept === true){
                 judgeResult.innerHTML = generate("success");
-                judgeResult.children[0].innerHTML += `<hr> <a href="/certificate?arg=${response.data.secret}&p=${problemKey}" class="alert-link">Click here to download the certificate</a>`
-                keySequence = [];
+                judgeResult.children[0].innerHTML += `<hr> <a href="/certificate?arg=${response.data.secret}&p=${problemKey}" class="alert-link">點擊查看</a>`
+                clearUserInput(false);
             }else{
                 judgeResult.innerHTML = generate("danger");
-                judgeResult.children[0].innerHTML += `<button class="btn btn-outline-primary btn-sm float-right" id="key-seq-reset">Reset</button>`
+                judgeResult.children[0].innerHTML += `<button class="btn btn-outline-primary btn-sm float-right" id="key-seq-reset">重置</button>`
                 document.getElementById('key-seq-reset').addEventListener('click', function() {
-                    keySequence = [];
-                    judgeResult.innerHTML = "";
+                    clearUserInput(true);
                 });
             }
         })
@@ -44,18 +67,18 @@ document.querySelectorAll('.weapon-btn').forEach(button => {
         // .finally(function () {
         //     keySequence = [];
         // })
-    });
-});
+}
 
 const judgeTable = {
-    info : "Judging...",
-    success: "Accepted!",
-    danger: "Failed...",
-    warning: "Error! Please retry or ask the administrator"
+    info : "等待系統回應...",
+    success: "你成功了！ 咦？這是什麼？",
+    danger: "這方法似乎行不通... 換個方法試試（？",
+    warning: "Error! Please retry or ask the administrator",
+    primary: `戰術準備：`
 };
 
 function generate(property){
-    const resultDisplay = `
+    let resultDisplay = `
         <div class="alert alert-${property}" role="alert">
             ${judgeTable[property]}
         </div>
